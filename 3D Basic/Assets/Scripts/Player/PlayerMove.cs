1283256628fe4af20dbb55bd3c9719bc4f0e1ad5
DevblueNotWorking <wooyeophan@gmail.com>
2021-03-12 15:09:09 +0900

    학교프젝

---
+++ 3D Basic/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float     speed = 10.0f;
+                     private Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        // GetAxis    -1  ~  1
+        // GetAxisRaw -1, 0, 1
+
+
+        rigid.velocity = transform.forward.normalized * z * speed;
+        rigid.rotation = rigid.rotation * Quaternion.Euler(0, x, 0);
+        
+
+    }
+}
