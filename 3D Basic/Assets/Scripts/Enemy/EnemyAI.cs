0d2ce475c31ccf2390f475e5121960e77eddeb52
Han not working these days <wooyeophan@gmail.com>
2021-04-26 16:39:53 +0900

    7교시 게임엔진

---
+++ 3D Basic/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    public EnemyState state = EnemyState.PATROL;
+
+    private Transform playerTr;
+
+    public float attackDist = 5.0f;
+    public float traceDist = 10.0f;
+    public float judgeDelay = 0.3f;
+    // 매 프래임마다 판단하면 엄청난 부하 걸림
+
+    public bool isDie = false;
+    private WaitForSeconds ws;
+    private MoveAgent moveAgent;
+
+    private void Awake()
+    {
+        moveAgent = GetComponent<MoveAgent>();
+        ws = new WaitForSeconds(judgeDelay);
+    }
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("PLAYER");
+        if(player != null)
+        {
+            playerTr = player.transform;
+        }
+    }
+
+    private void OnEnable() // Start 보다 빠름
+    {
+        StartCoroutine(CheckState());
+        StartCoroutine(Action());
+    }
+
+
+    // 자신상태 채크 코루틴
+    IEnumerator CheckState()
+    {
+        while(!isDie)
+        {
+            if(state == EnemyState.DIE)
+            {
+                yield break; // 코루틴 깨짐
+            }
+            if (playerTr == null)
+                yield return ws;
+
+            float dist = (playerTr.position - transform.position).sqrMagnitude;
+
+            if (dist <= attackDist * attackDist)
+            {
+                state = EnemyState.ATTACK;
+            }
+            else if (dist <= traceDist * traceDist)
+            {
+                state = EnemyState.TRACE;
+            }
+            else
+            {
+                state = EnemyState.PATROL;
+            }
+
+            yield return ws;
+        }
+    }
+
+
+    // 상태 기반 액션 코루팅
+    IEnumerator Action()
+    {
+        while (!isDie)
+        {
+            yield return ws;
+            switch (state)
+            {
+                case EnemyState.PATROL:
+                    moveAgent.patrolling = true;
+                    break;
+
+                case EnemyState.TRACE:
+                    moveAgent.traceTarget = playerTr.position;
+                    break;
+
+                case EnemyState.ATTACK:
+                    moveAgent.Stop();
+                    break;
+
+                case EnemyState.DIE:
+                    moveAgent.Stop();
+                    break;
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- 3D Basic/Assets/Scripts/Enemy/MoveAgent.cs
+++ 3D Basic/Assets/Scripts/Enemy/MoveAgent.cs
@@ -1,18 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+// 이 스크립트 붙이면 자동으로 NavMeshAgent 붙음
+[RequireComponent(typeof(NavMeshAgent))]
 public class MoveAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform wayPointGroup;
+    // <> <== C#에선 제네릭 (C++ == Template)
+    private List<Transform> wayPoints = new List<Transform>();
+
+    public int nextIndex = 0;
+    private NavMeshAgent agent;
+
+    private readonly float patrolSpeed = 1.5f;
+    private readonly float traceSpeed = 4.0f;
+    private bool _patrolling;
+    public bool patrolling
     {
-        
+        get { return _patrolling; }
+        set
+        {
+            _patrolling = value;
+            if (_patrolling)
+            {
+                agent.speed = patrolSpeed;
+                MoveWayPoint();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 _traceTarget;
+    public Vector3 traceTarget
     {
-        
+        get { return _traceTarget; }
+        set
+        {
+            _traceTarget = value;
+            agent.speed = traceSpeed;
+            TraceTarget(_traceTarget);
+        }
+    }
+
+    private void TraceTarget(Vector3 pos)
+    {
+        if (agent.isPathStale) return;
+        agent.destination = pos;
+        agent.isStopped = false;
+    }
+
+    public void Stop()
+    {
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+        _patrolling = false;
+    }
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        // 목적지 다가갈 수록 자동으로 속도 줄이는 옵션 = false;
+    }
+
+    void Start()
+    {
+        wayPointGroup.GetComponentsInChildren<Transform>(wayPoints); // 자기 자신도 가져옴
+        wayPoints.RemoveAt(0); // <= 부모의 것 삭제
+        MoveWayPoint();
+    }
+
+    private void MoveWayPoint()
+    {
+        // isPathStale == 경로 준비 안 되어있으면 true
+        if (agent.isPathStale) return;
+        agent.destination = wayPoints[nextIndex].position;
+        agent.isStopped = false; // 에이전트 on
+    }
+
+    private void Update()
+    {
+        if (!_patrolling) return;
+
+        if(agent.velocity.sqrMagnitude >= 0.04f && agent.remainingDistance <= 0.5f)
+        {
+            nextIndex = (++nextIndex) % wayPoints.Count; // 0 1 2 3 0 1 2 3 0 1 2 3...
+            MoveWayPoint();
+        }
     }
 }
